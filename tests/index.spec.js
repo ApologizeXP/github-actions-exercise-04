259c47f891472bea0beae5d1b528a96ab752d9ca
apologizexp <sopondclay@gmail.com>
2021-10-08 20:14:23 +0700

    TEst

--- tests/index.spec.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest')
-const app = require('../app')
-
-describe('GET /', () => {
-    it('responds with "Hello world!!!"', (done) => {
-        request(app).get('/').expect({message: 'Hello world!'}, done)
-    })
-})
-
